b7f7f8d99948568016ec16815dd587c2dea223e6
AcrylonitrileButadieneStyrene <AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-12-30 20:34:26 +0000

    deploy: 396c8c220fdee31de932ec4ba1c266e46e17eacd

---
+++ yno_vc.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name      YNO Voice Chat
+// @version   0.0.1
+// @license   MIT OR Apache-2.0
+// @source    https://github.com/AcrylonitrileButadieneStyrene/yno-vc
+// @match     *://ynoproject.net/*
+// @resource  wasm  http://localhost:3000/dist/yno_vc.wasm
+// @grant     GM.getResourceUrl
+// @grant     GM.getValue
+// @grant     GM.setValue
+// @grant     unsafeWindow
+// ==/UserScript==
