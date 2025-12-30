b0c334644009ac1e2607f4764105450799f3f9c6
AcrylonitrileButadieneStyrene <AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-12-30 21:01:16 +0000

    deploy: b7661e9a4f357b469d58f3ce6aa4063851a46edc

--- yno_vc.meta.js
+++ yno_vc.meta.js
@@ -1,12 +1,15 @@
 // ==UserScript==
-// @name      YNO Voice Chat
-// @version   0.0.1
-// @license   MIT OR Apache-2.0
-// @source    https://github.com/AcrylonitrileButadieneStyrene/yno-vc
-// @match     *://ynoproject.net/*
-// @resource  wasm  http://localhost:3000/dist/yno_vc.wasm
-// @grant     GM.getResourceUrl
-// @grant     GM.getValue
-// @grant     GM.setValue
-// @grant     unsafeWindow
+// @name         YNO Voice Chat
+// @version      0.0.1
+// @license      MIT OR Apache-2.0
+// @source       https://github.com/AcrylonitrileButadieneStyrene/yno-vc
+// @supportURL   https://github.com/AcrylonitrileButadieneStyrene/yno-vc/issues
+// @downloadURL  https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.user.js
+// @updateURL    https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.meta.js
+// @match        *://ynoproject.net/*
+// @resource     wasm  https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.wasm
+// @grant        GM.getResourceUrl
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        unsafeWindow
 // ==/UserScript==
