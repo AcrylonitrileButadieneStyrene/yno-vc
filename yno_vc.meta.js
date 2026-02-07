08fb2f159a85803b30311ef368523f6dc7eabd2e
AcrylonitrileButadieneStyrene <AcrylonitrileButadieneStyrene@users.noreply.github.com>
2026-02-07 13:56:44 +0000

    deploy: f6b489dc7d047176776969e374c05cec494cf151

--- yno_vc.meta.js
+++ yno_vc.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         YNO Voice Chat
-// @version      0.0.2
+// @version      0.0.3
 // @license      MIT OR Apache-2.0
 // @source       https://github.com/AcrylonitrileButadieneStyrene/yno-vc
 // @supportURL   https://github.com/AcrylonitrileButadieneStyrene/yno-vc/issues
 // @downloadURL  https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.user.js
 // @updateURL    https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.meta.js
 // @match        *://ynoproject.net/*
-// @resource     wasm  https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.wasm
+// @resource     wasm  https://raw.githubusercontent.com/AcrylonitrileButadieneStyrene/yno-vc/refs/heads/builds/yno_vc.wasm?version=0.0.3
 // @grant        GM.getResourceUrl
 // @grant        GM.getValue
 // @grant        GM.setValue
